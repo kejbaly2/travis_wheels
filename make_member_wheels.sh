f0bd5231dba742e73df13c271efe80c96e4f5dba
Chris Ward <cward@redhat.com>
2015-08-21 09:58:06 +0200

    member wheels

---
+++ make_member_wheels.sh
@@ -0,0 +1,11 @@
+# Aggregate the exit status so build fails if a wheel is not created:
+EXIT_STATUS=0
+pip wheel --wheel-dir=./wheelhouse --no-deps requests || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse --no-deps requests-kerberos || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse --no-deps pyyaml || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse --no-deps pytest || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse --no-deps instructions || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse --no-deps PyGithub || EXIT_STATUS=$?
+
+exit $EXIT_STATUS
+
