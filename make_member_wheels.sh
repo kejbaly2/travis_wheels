b8e5462d83aba19f16cafdba6bca1405d30459b7
Chris Ward <cward@redhat.com>
2015-08-21 14:08:29 +0200

    added pyrestful and pytest-pep8

--- make_member_wheels.sh
+++ make_member_wheels.sh
@@ -4,8 +4,9 @@ pip wheel --wheel-dir=./wheelhouse --no-deps requests || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse --no-deps requests-kerberos || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse --no-deps pyyaml || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse --no-deps pytest || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse --no-deps pytest-pep8 || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse --no-deps instructions || EXIT_STATUS=$?
 pip wheel --wheel-dir=./wheelhouse --no-deps PyGithub || EXIT_STATUS=$?
+pip wheel --wheel-dir=./wheelhouse --no-deps pyrestful || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
-
